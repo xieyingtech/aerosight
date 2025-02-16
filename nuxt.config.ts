e30b4722db58a34dc3ce30e6a04a104629f17bed
Saurlax <saurlax@qq.com>
2025-02-17 00:38:53 +0800

    feat: add login page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  modules: ["nuxt-auth-utils", "@prisma/nuxt", "@nuxt/ui"],
+  app: {
+    head: {
+      title: "协盈穹目",
+    },
+  },
+  prisma: {
+    installStudio: false,
+  },
+});
---
+++ server/api/login.ts
@@ -0,0 +1,30 @@
+import bcrypt from "bcryptjs";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  username: z.string().min(3),
+  password: z.string().min(8),
+});
+
+export default defineEventHandler(async (event) => {
+  const { username, password } = await readValidatedBody(
+    event,
+    bodySchema.parse
+  );
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    throw createError({
+      statusCode: 401,
+      message: "Invalid username or password",
+    });
+  }
+
+  await setUserSession(event, { user: { username } });
+  return { username };
+});
