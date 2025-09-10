6b1719ad7275bd866c4a79803d4b64bd9147cba5
Saurlax <saurlax@qq.com>
2025-09-10 15:51:24 +0800

    chore: update to nuxt ui4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   modules: [
     "nuxt-auth-utils",
+    "nuxt-authorization",
     "nuxt-echarts",
-    "@nuxtjs/leaflet",
     "@vueuse/nuxt",
     "@nuxt/ui",
   ],
-  runtimeConfig: {
-    public: {
-      tiandituApikey: "",
-    },
-  },
   echarts: {
     charts: ["BarChart"],
     components: ["DatasetComponent", "GridComponent", "TooltipComponent"],
