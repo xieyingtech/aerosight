8d28d54ede7fc56d7af3bb9789d6cbb3611547aa
Saurlax <saurlax@qq.com>
2026-03-08 23:09:58 +0800

    feat: import maplibre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@vueuse/nuxt", "nuxt-echarts"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/supabase",
+    "@vueuse/nuxt",
+    "nuxt-echarts",
+    "nuxt-maplibre",
+  ],
   echarts: {
     charts: ["BarChart"],
     components: ["DatasetComponent", "GridComponent", "TooltipComponent"],
